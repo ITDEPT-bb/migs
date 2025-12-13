3c7a1d29d54ccd63ce91c242cc184956bb4a6301
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2025-12-13 09:51:34 +0800

    chaning logos of partners

--- resources/js/Components/LandingPage/Navbar.jsx
+++ resources/js/Components/LandingPage/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = ({ auth }) => {
 				  <a href="#" className="flex flex-col items-center group relative">
 				    <img
 				      src={ESWFALogo}
-				      className="h-auto w-28 sm:w-36 md:w-48 bg-primary dark:bg-transparent px-3 py-2 rounded"
+				      className="h-auto w-28 sm:w-36 md:w-48"
 				      alt="World Esports and Digital Sports Academy Logo"
 				    />
 				    <span className="absolute top-full mb-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black text-white text-xs rounded px-2 py-1 whitespace-nowrap">
@@ -70,7 +70,7 @@ const Navbar = ({ auth }) => {
 				  >
 				    <img
 				      src={ESWFLogo}
-				      className="h-auto w-24 sm:w-28 md:w-36 bg-primary dark:bg-transparent px-3 py-3.5 rounded"
+				      className="h-auto w-24 sm:w-28 md:w-36"
 				      alt="ESWF Logo"
 				    />
 				    <span className="absolute top-full mb-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black text-white text-xs rounded px-2 py-1 whitespace-nowrap">
